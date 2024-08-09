7a4d562f5942aefa5c1718ce55b4d8a78dc9cd08
Derick Mathews <derickdennismathews@gmail.com>
2024-08-09 16:29:54 -0700

    fleshed out code still having error making command instance

--- Teledyne/ExternalControl/connect_serial.py
+++ Teledyne/ExternalControl/connect_serial.py
@@ -1,5 +1,7 @@
 import serial.tools.list_ports as port_list
+import time
 import serial 
+import threading
 
 from mmp_defs import *  # Import the command class
 
@@ -12,31 +14,43 @@ def find_ports():
     for p in ports:
         try:
             print("trying", p)
-            #            if(p[len(p)-3:]=="n/a"):
-            #                continue
+            # filter out non possible modem connections
             if (p.__str__()[-3:] == 'n/a'):
                 print("N/A port")
                 continue
+
+
+            #try connecting to the serial port 
             ser = serial.Serial(p.device, 9600, timeout=1)
             if ser.is_open:
                 print("Connected to", p.device)
                 return ser 
             ser.close()
+
         except serial.SerialException as exception:
             print("failed to connect to", p.device, exception)
 
 
 
 def send_ranging_command(serial_connection, modem_id):
+    #print type of mmp_remote_t
     print(dir(mmp_remote_t))
 
-    my_message = mmp_remote_t.__call__()
+    for _ in range(100):
+        ranging_command = mmp_remote_t()
+        
+        serialized_ranging_command = ranging_command.serialize()
+
+        serial_connection.write(serialized_ranging_command)
+
+        time.sleep(5)
 
 
 
-def connect_port(serial_connection):
+def display_input(serial_connection):
     if not  serial_connection:
-        print("Bad Connection")
+        print("Error: Bad Serial Connection")
+        raise
         return
     try:
         while True:
@@ -49,6 +63,7 @@ def connect_port(serial_connection):
             send_ranging_command(serial_connection, 1)
             time.sleep(5)
 
+
     except KeyboardInterrupt:
         print("Keyboard Interrupt\n Exiting...")
     finally:
@@ -56,4 +71,14 @@ def connect_port(serial_connection):
 
 
 ser = find_ports()
-connect_port(ser)
+display_input_thread = threading.Thread(target=display_input, args=(ser))
+send_range_thread = threading.Thread(target=send_ranging_command, args=(ser, 1))
+
+#display input while sending range command
+display_input_thread.start()
+send_range_thread.start()
+
+
+#close threads
+display_input_thread.join()
+send_range_thread.join()
